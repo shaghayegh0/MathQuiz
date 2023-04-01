2f7ed408cab59a3a828fff1e997354da17dcb7ca
Jordan Dixon <jdixon@konradgroup.com>
2023-04-01 18:02:43 -0400

    update point, random nextQ based on answer for algebra

--- quiz/src/App.js
+++ quiz/src/App.js
@@ -1,134 +1,117 @@
 import React, { useState, useEffect } from "react";
 import "./App.css";
 
-
 function App() {
   const [questions, setQuestions] = useState([]);
-  const [correctAnswer, setCorrectAnswer] = useState(false);
   const [currentQuestion, setCurrentQuestion] = useState({});
-  const [points, setPoints] = useState([]);
+  const [visitedQuestions, setVisitedQuestions] = useState([]);
+  const [selectedAnswer, setSelectedAnswer] = useState("");
+  const [points, setPoints] = useState(0);
+  const [count , setCount] = useState(0);
 
   useEffect(() => {
     fetch("/Database.json")
-    .then(response => response.json())
-    .then(data => {
-      if (!Array.isArray(data)) {
-        data = [data];
-      }
-      var result = {};
-      data.forEach(question => {
-        result = question;
-      });
-      setQuestions(result.algebra);
-      // Set first question
-      console.log(result.algebra);
-    })
-    .catch(error => console.error(error));
+      .then((response) => response.json())
+      .then((data) => {
+        if (!Array.isArray(data)) {
+          data = [data];
+        }
+        var result = {};
+        data.forEach((question) => {
+          result = question;
+        });
+        setQuestions(result.algebra);
+        console.log(result.algebra);
+      })
+      .catch((error) => console.error(error));
   }, []);
 
-  
+  useEffect(() => {
+    setCurrentQuestion(
+      questions
+        .filter(
+          (question) =>
+            question.level === 1 && !visitedQuestions.includes(question)
+        )
+        .sort(() => 0.5 - Math.random())
+        .slice(0, 1)[0]
+    );
+  }, [questions]);
 
-  // Randomly select 1 questions from the questions array
-  const randomQuestions = questions.sort(() => 0.5 - Math.random()).slice(0, 1);
-  
-  
-  const handleNextQuestion = () => {
-    
-
-    // If the answer was correct
-    if (correctAnswer) {
-
-      //set correctAnswer to false
-      //to select a new random question
-      setCorrectAnswer(true);
-
-      //add the q.level to points
-      setPoints([...points, currentQuestion.level]);
-
-      //filter questions based on currentQuestion.level
-      const filteredQuestions = questions.filter(
-        (question) => question.level === currentQuestion.level
-      );
-
-      //next questions are from higher levels
-      const nextQuestion =
-        filteredQuestions[
-          Math.floor(Math.random() * filteredQuestions.length)
-        ];
-
-      //update current Quesiton2  
-      setCurrentQuestion(nextQuestion);
-    }
-
-
-    // Otherwise, keep the current question and wait for the user to submit a correct answer
-     else {
-      setCorrectAnswer(false);
-      setPoints([...points, 0]);
-      
-      const filteredQuestions = questions.filter(
-        (question) => {
-          console.log(question);
-          console.log(currentQuestion);
-          return question.level <= currentQuestion.level
-        }
-      );
-      const nextQuestion =
-        filteredQuestions[
-          Math.floor(Math.random() * filteredQuestions.length)
-        ];
-      setCurrentQuestion(nextQuestion);
-    }
-
+  const handleAnswerChange = (event) => {
+    setSelectedAnswer(event.target.value);
   };
 
+  const handleNextButtonClick = () => {
+    setCount(count+1);
+    update(selectedAnswer);
+    if (visitedQuestions.length < 9) {
+      const nextQuestion = questions
+        .filter(
+          (question) =>
+            (selectedAnswer === currentQuestion.a &&
+              question.level >= currentQuestion.level &&
+              !visitedQuestions.includes(question)) ||
+            (selectedAnswer !== currentQuestion.a &&
+              question.level <= currentQuestion.level &&
+              !visitedQuestions.includes(question))
+        )
+        .sort(() => 0.5 - Math.random())
+        .slice(0, 1)[0];
+      setCurrentQuestion(nextQuestion);
+      setVisitedQuestions([...visitedQuestions, currentQuestion]);
+      setSelectedAnswer("");
+    } else {
+      // quiz is done
+      // show the points
+      alert(`Quiz is done! You got ${points} points.`);
+      // new page
+      window.location.reload();
+    }
+  };
+
+  const update = (answer) => {
+    if (answer === currentQuestion.a) {
+      setPoints(points + currentQuestion.level);
+    } else {
+      setPoints(points + 0);
+    }
+    console.log("Updating with answer:", answer);
+  };
 
   return (
     <div>
-      
-      <div>Points: {points.join(", ")}</div>
-
-      {/* Use currentQuestion instead of a map */}
-
-
-      {randomQuestions.map((question) => (      
-        <div key={question.q}>
-          <h2 class='ques'>Q:{question.q} (level: {question.level})</h2> 
-          {/* <h3>a:{question.a}</h3> */}
-          
-          <ul
-            class="options"
-            style={{ listStyle: "none", padding: 10 }}
-          >
-            {question.answers.map((answer) => (
+      <p>Question {count} out of 10</p>
+      {currentQuestion && (
+        <div key={currentQuestion.q}>
+          <h2 class='ques'>
+            Q:{currentQuestion.q} (level: {currentQuestion.level})
+          </h2>
+          {/* <h3>a:{currentQuestion.a}</h3> */}
+          <ul class='options'
+          style={{ listStyle: "none", padding: 10 }}>
+            {Array.isArray(currentQuestion.answers) &&
+            currentQuestion.answers.map((answer) => (
               <li key={answer}>
                 <label>
-                  <input
-                    type="checkbox"
-                    name={answer}
-                    value={answer}
-                    onChange={() => {
-                      // Check if the answer is correct
-                      //update the state variable accordingly
-                      // if (answer === question.a) {
-                      //   setCorrectAnswer(true);
-                      // }
-                    }}
-                  />
-                  {answer}
+                <input
+                type="radio"
+                name="answer"
+                value={answer}
+                checked={selectedAnswer === answer}
+                onChange={handleAnswerChange}
+                />
+                {answer}
                 </label>
               </li>
             ))}
           </ul>
 
-
-
-          <button class='next' type="submit" 
-          onClick={handleNextQuestion}>
-            next
-          </button>
         </div>
-      ))}
+      )}
+      <button class='next' type="button" onClick={handleNextButtonClick}>
+        next
+      </button>
     </div>
   );
 }
